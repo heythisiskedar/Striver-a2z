6e03d8f1e36edb337a407b906b4627264808e809
Kedar Patel <kedar29112007@gmail.com>
2026-06-09 20:32:06 +0530

    Small changes

--- src/arrays/MediumProblems.java
+++ src/arrays/MediumProblems.java
@@ -575,6 +575,7 @@ public class MediumProblems {
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 index = i;
+
                 break;
             }
         }
@@ -587,6 +588,7 @@ public class MediumProblems {
         for (int i = n - 1; i > index; i++) {
             if (nums[i] > nums[index]) {
                 swap(nums, i, index);
+                
                 break;
             }
         }
