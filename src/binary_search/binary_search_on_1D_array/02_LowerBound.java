b5a06629a22d95618f1d81ced152de0806ceabeb
Kedar Patel <kedar29112007@gmail.com>
2026-06-11 19:36:47 +0530

    Add Find Lower Bound of an array problem, Optimal approach

--- src/binary_search/binary_search_on_1D_array/02_LowerBound.java
+++ src/binary_search/binary_search_on_1D_array/02_LowerBound.java
@@ -4,7 +4,7 @@ class LowerBound {
     public static void main(String[] args) {
         int[] nums = {1, 2, 2, 3};
 
-        int result = lowerBoundBrute(nums, 2);
+        int result = lowerBoundOptimal(nums, 2);
         System.out.println(result);
     }
 
@@ -16,5 +16,24 @@ class LowerBound {
         }
 
         return nums.length;
+    }
+
+    static int lowerBoundOptimal(int[] nums, int x) {
+        int low = 0;
+        int high = nums.length - 1;
+        int minIndex = nums.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= x) {
+                minIndex = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return minIndex;
     }
 }
