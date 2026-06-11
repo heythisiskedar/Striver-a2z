a962bc39f9df6abea1d3d18c92f0d1b9ec90954c
Kedar Patel <kedar29112007@gmail.com>
2026-06-11 19:43:51 +0530

    Add Recursive Binary Search in binary_search

--- src/binary_search/binary_search_on_1D_array/01_BinarySearchToSearchTarget.java
+++ src/binary_search/binary_search_on_1D_array/01_BinarySearchToSearchTarget.java
@@ -3,7 +3,7 @@ package binary_search.binary_search_on_1D_array;
 class BinarySearchToSearchTarget {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 5, 10, 11, 14};
-        int result = binarySearch(nums, 5);
+        int result = binarySearchRecursive(nums, 5, 0, nums.length - 1);
         System.out.println(result);
     }
 
@@ -24,5 +24,21 @@ class BinarySearchToSearchTarget {
         }
 
         return -1;
+    }
+
+    static int binarySearchRecursive(int[] nums, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            binarySearchRecursive(nums, target, low, mid - 1);
+        }
+
+        return binarySearchRecursive(nums, target, mid + 1, high);
     }
 }
