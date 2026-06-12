1ced0a587aac646668f33bf78743065a4d907eb9
Kedar Patel <kedar29112007@gmail.com>
2026-06-12 19:53:05 +0530

    Add Floor and Ceiling problem, binary search approach

---
+++ src/binary_search/binary_search_on_1D_array/FloorAndCeiling.java
@@ -0,0 +1,47 @@
+package binary_search.binary_search_on_1D_array;
+
+public class FloorAndCeiling {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 4, 7, 8, 10};
+        foorAndCeiling(nums,5);
+    }
+
+    // floor - The floor of x is the largest element in the array which is smaller than or equal to x
+    // ceil - The ceiling of x is the smallest element in the array greater than or equal to x
+    static void foorAndCeiling(int[] nums, int x) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+
+        int floor = -1;
+        int ceil = -1;
+
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] <= x) {
+                floor = nums[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= x) {
+                ceil = nums[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println(floor + " " + ceil);
+    } 
+}
