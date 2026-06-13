34ac5812f62f06fd33073873c4cdfa716a65db04
Kedar Patel <kedar29112007@gmail.com>
2026-06-13 20:36:13 +0530

    Count Occurences in Sorted Array Problem, Brute and Optimal approach

---
+++ src/binary_search/binary_search_on_1D_array/07_CountOccurrences.java
@@ -0,0 +1,58 @@
+package binary_search.binary_search_on_1D_array;
+
+// Problem Statement: You are given a sorted array containing N integers and a number X, you have to find the occurrences of X in the given array.
+class CountOccurences {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 2, 2, 2, 3};
+
+        int ans = countOccurencesOptimal(nums, 2);
+        System.out.println(ans);
+    }
+
+    static int countOccurencesBrute(int[] nums, int target) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static int countOccurencesOptimal(int[] nums, int target) {
+    int[] ans = {-1, -1};
+
+    ans[0] = searchBound(nums, target, true);
+    ans[1] = searchBound(nums, target, false);
+
+    return ans[1] - ans[0] + 1;
+    }
+
+    private static int searchBound(int[] nums, int target, boolean isFirst) {
+        int low = 0;
+        int high = nums.length - 1;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                index = mid;
+
+                if (isFirst) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return index;
+    }
+}
