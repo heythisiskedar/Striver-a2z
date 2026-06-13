d64d3cf474085bfc5803e6bda71ead7b07191d6d
Kedar Patel <kedar29112007@gmail.com>
2026-06-13 21:36:27 +0530

    Search in Rotated Array problem, Optimal approach

--- src/binary_search/binary_search_on_1D_array/08_SearchInRotatedArray.java
+++ src/binary_search/binary_search_on_1D_array/08_SearchInRotatedArray.java
@@ -4,7 +4,7 @@ class SearchInRotatedArray {
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
 
-        int ans = searchInRotatedArrayBrute(nums, 0);
+        int ans = searchInRotatedArrayOptimal(nums, 0);
         System.out.println(ans);
     }
 
@@ -12,6 +12,35 @@ class SearchInRotatedArray {
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == target) {
                 return i;
+            }
+        }
+
+        return -1;
+    }
+
+    static int searchInRotatedArrayOptimal(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target <= nums[high] && nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
 
