37c1b7642feaf0d0078bec4985381726a2f784a0
Kedar Patel <kedar29112007@gmail.com>
2026-06-16 20:44:39 +0530

    Add solution for finding single element in sorted array, Brute & Optimal approach

---
+++ src/binary_search/binary_search_on_1D_array/12_SingleElementInSortedArray.java
@@ -0,0 +1,49 @@
+package binary_search_on_1D_array;
+
+class Solution {
+
+    public int singleNonDuplicateBrute(int[] nums) {
+      int ans = 0;
+
+      for (int num : nums) {
+        ans ^= num;
+      }
+
+      return ans;
+    }
+  
+    public int singleNonDuplicateOptimal(int[] nums) {
+        int n = nums.length;
+        
+        if (n == 1) {
+            return nums[0];
+        }
+        
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+        
+        int low = 1;
+        int high = n - 2;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            
+            if (mid % 2 == 0 && nums[mid] == nums[mid + 1] || mid % 2 == 1 && nums[mid] == nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
