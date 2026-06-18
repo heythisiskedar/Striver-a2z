9700049e76937f463e52232171e459af66860c38
Kedar Patel <kedar29112007@gmail.com>
2026-06-18 21:14:47 +0530

    Add binary search solution for square root calculation

---
+++ src/binary_search/binary_search_on_answers/01_SquareRootOfNumber.java
@@ -0,0 +1,26 @@
+package binary_search_on_answers;
+
+class Solution {
+  static int floorSqrt(int num) {
+        if (num == 0 || num == 1) {
+            return num;
+        }
+
+        int low = 1;
+        int high = num / 2;
+        int ans = 0;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (mid * mid <= num) {
+                ans = (int) mid;
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+
+        return ans;
+  }
+}
