d04b151f71dd17b1ac4f78c8c1dd578dac474c09
Kedar Patel <kedar29112007@gmail.com>
2026-06-20 17:54:27 +0530

    Implement smallest divisor binary search solution

---
+++ src/binary_search/binary_search_on_answers/05_FindTheSmallestDivisor.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low = 1;
+        int high = 0;
+        for (int num : nums) {
+            high = Math.max(num, high);
+        }
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (checkDivisor(nums, mid, threshold)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private boolean checkDivisor(int[] nums, int divisor, int threshold) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor;
+            if (sum > threshold) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
