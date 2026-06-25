dc4611a196f37b90904cc5c5855e514d477793e9
Kedar Patel <kedar29112007@gmail.com>
2026-06-25 17:47:17 +0530

    Add binary search solution for Split Array problem

---
+++ src/binary_search/binary_search_on_answers/09_SplitArrayLargestSum.java
@@ -0,0 +1,43 @@
+package binary_search_on_answers;
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low = 0;
+        int high = 0;
+
+        for (int num : nums) {            
+            low = Math.max(num, low);
+            high += num;
+        }
+        
+        int ans = low;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (subarrayCount(nums, mid) <= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int subarrayCount(int[] nums, int maxSum) {
+        int subarrays = 1;
+        int currentSum = 0;
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                subarrays++;
+                currentSum = num;
+            } else {
+                currentSum += num;
+            }
+        }
+
+        return subarrays;
+    }
+}
